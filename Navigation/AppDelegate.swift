14b8962d7bce45699dc25bb9f4c60f2cbbb76ac7
Kirill Feev <kirill.feev@mail.ru>
2022-05-21 14:43:43 +0300

    Add Navigation proj

---
+++ Navigation/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Navigation
+//
+//  Created by Кирилл on 04.05.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Navigation/MainTabBarViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainTabBarViewController.swift
+//  Navigation
+//
+//  Created by Кирилл on 21.04.2022.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    let feedVc = FeedViewController()
+//    let profileVc = ProfileViewController()
+    let profileVc = LogInViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+        setupControllers()
+    }
+
+    private func setupControllers() {
+        
+        let feedNavController = UINavigationController(rootViewController: feedVc)
+        feedVc.tabBarItem.title = "Лента"
+        feedVc.tabBarItem.image = UIImage(systemName: "bolt")
+        
+        let profileNavController = UINavigationController(rootViewController: profileVc)
+        profileVc.tabBarItem.title = "Профиль пользователя"
+        profileVc.tabBarItem.image = UIImage(systemName: "face.smiling")
+        profileVc.navigationItem.title = "Profile"
+        profileNavController.navigationBar.isHidden = true
+        
+        viewControllers = [feedNavController, profileNavController]
+    }
+}
